8c4f8671800c6f57ee87d12274ee042ab891457e
Patrik Oliveira <patrikro.oliveira@gmail.com>
2023-04-14 20:06:06 -0300

    feat(product): :sparkles: add jwt

---
+++ product-api/src/main/java/br/com/comunicacaomicrosservicosudemy/productapi/modules/jwt/service/JwtService.java
@@ -0,0 +1,50 @@
+package br.com.comunicacaomicrosservicosudemy.productapi.modules.jwt.service;
+
+import br.com.comunicacaomicrosservicosudemy.productapi.config.exception.AuthenticationException;
+import br.com.comunicacaomicrosservicosudemy.productapi.modules.jwt.dto.JwtResponse;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
+
+@Service
+public class JwtService {
+    private static final String BEARER = "bearer ";
+
+    @Value("${app-config.secrets.api-secret}")
+    private String apiSecret;
+
+    public void validateAuthorization(String token) {
+        try {
+            var accessToken = extractToken(token);
+            var claims = Jwts
+                    .parserBuilder()
+                    .setSigningKey(Keys.hmacShaKeyFor(apiSecret.getBytes()))
+                    .build()
+                    .parseClaimsJws(accessToken)
+                    .getBody();
+
+            var user = JwtResponse.getUser(claims);
+            if (isEmpty(user) || isEmpty(user.getId())) {
+                throw new AuthenticationException("The user is not valid.");
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new AuthenticationException("Error while trying to proccess the Access Token.");
+        }
+    }
+
+    private String extractToken(String token) {
+        if (isEmpty(token)) {
+            return null;
+        }
+        if (token.toLowerCase().contains(BEARER)) {
+            token = token.toLowerCase();
+            token = token.replace(BEARER, Strings.EMPTY);
+        }
+        return token;
+    }
+}
