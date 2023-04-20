a60de88e76f52d7a4d61d412fb9865a127ff29e6
Patrik Oliveira <patrikro.oliveira@gmail.com>
2023-04-19 22:51:36 -0300

    feat(sales): :sparkles: cria listener da aplicacao

--- sales-api/src/config/rabbitmq/rabbitConfig.js
+++ sales-api/src/config/rabbitmq/rabbitConfig.js
@@ -1,4 +1,5 @@
 import amqp from "amqplib/callback_api.js";
+import { listenToSalesConfirmationQueue } from "../../modules/sales/rabbitmq/salesConfirmationListener.js";
 import {
   PRODUCT_TOPIC,
   PRODUCT_STOCK_UPDATE_QUEUE,
@@ -9,7 +10,7 @@ import {
 
 import { RABBIT_MQ_URL } from "../constants/secrets.js";
 
-const HALF_SECOND = 500;
+const TWO_SECONDS = 2000;
 const HALF_MINUTE = 30000;
 const CONTAINER_ENV = "container";
 
@@ -47,8 +48,12 @@ async function connectRabbitMqAndCreateQueues() {
 
     setTimeout(function () {
       connection.close();
-    }, HALF_SECOND);
+    }, TWO_SECONDS);
   });
+
+  setTimeout(function () {
+    listenToSalesConfirmationQueue();
+  }, TWO_SECONDS);
 }
 
 function createQueue(connection, queue, routingKey, topic) {
