a8e40154c7270d94fa739ee48b1a835494eea5fa
Patrik Oliveira <patrikro.oliveira@gmail.com>
2023-04-25 16:41:08 -0300

    feat(auth): :sparkles: implementa tracing de requisicoes entre servicos

--- auth-api/app.js
+++ auth-api/app.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import * as db from "./src/config/db/initialData.js";
 import userRoutes from "./src/modules/user/routes/UserRoutes.js";
+import tracing from "./src/config/tracing.js";
 
 const app = express();
 const env = process.env;
@@ -9,8 +10,8 @@ const PORT = env.PORT || 8080;
 
 db.createInitialData();
 
+app.use(tracing);
 app.use(express.json());
-
 app.use(userRoutes);
 
 app.get("/api/status", (req, res) => {
